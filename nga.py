ead24ed374311d5365d0ae646cbafd0d84bc6878
sieuthifun-binh <sieuthifun@gmail.com>
2026-06-05 23:12:49 +0700

    nga.py

--- nga.py
+++ nga.py
@@ -340,7 +340,7 @@ with tab6:
                                     'https://api.remove.bg/v1.0/removebg',
                                     files={'image_file': uploaded_bg_img.getvalue()},
                                     data={'size': 'auto'},
-                                    headers={'X-Api-Key': st.secrets["PybugvXzq8CQjm4tUUVxVPH1"]},
+                                    headers={'X-Api-Key': st.secrets["REMOVE_BG_API_KEY"]},
                                 )
                                 if response.status_code == 200:
                                     img_data = response.content
